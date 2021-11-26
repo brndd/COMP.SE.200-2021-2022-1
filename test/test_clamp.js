6b88c79459b122790150bc6738f88d970b520084
brndd <burneddi@gmail.com>
2021-11-26 02:51:00 +0200

    Improve clamp.js test coverage.

--- test/test_clamp.js
+++ test/test_clamp.js
@@ -11,4 +11,16 @@ describe('clamp()', function() {
     it('should return NaN when clamping a string to -5..5', function() {
        expect(clamp("this is not a number", -5, 5)).to.be.NaN;
     });
+    it('should return 0 when clamping -5 to NaN..5', function() {
+       expect(clamp(-5, NaN, 5)).to.equal(0); 
+    });
+    it('should return 0 when clamping 10 to -5..NaN', function() {
+       expect(clamp(10, -5, NaN)).to.equal(0);
+    });
+    it('should return 5 when clamping 10 to 5..5', function () {
+       expect(clamp(10, 5, 5)).to.equal(5); 
+    });
+    it('should return 5 when clamping -5 to 5..5', function () {
+       expect(clamp(-5, 5, 5)).to.equal(5); 
+    });
 });
