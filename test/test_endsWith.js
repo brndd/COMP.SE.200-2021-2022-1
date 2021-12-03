31c3b91f28433ee368596f574bf624e09270b32f
vechtomo <vladimir.vechtomov@tuni.fi>
2021-12-03 21:57:20 +0200

    added tests for endsWith.js

---
+++ test/test_endsWith.js
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import endsWith from '../src/endsWith.js';
+
+describe('endsWith()', function() {
+    // Example tests
+    it("should return true with params 'abc','c'", function() {
+        expect(endsWith('abc','c')).to.be.true;
+    });
+    it("should return false with params 'abc','b'", function() {
+        expect(endsWith('abc','b')).to.be.false;
+    });
+    it("should return true with params 'abc','b', 2", function() {
+        expect(endsWith('abc','b',2)).to.be.true;
+    });
+    it("should return true with params 'abc','b', 3", function() {
+        expect(endsWith('abc','c',3)).to.be.true;
+    });
+
+    //Empty string
+    it("should return false with params '','b'", function() {
+        expect(endsWith('','b')).to.be.false;
+    });
+
+    //Empty target
+    it("should return true with params 'abc',''", function() {
+        expect(endsWith('abc', '')).to.be.true;
+    });
+    it("should return false with params 'abc','',1", function() {
+        expect(endsWith('abc', '', 1)).to.be.false;
+    });
+    //Negative index
+    it("should return false with params 'abc','',-1", function() {
+        expect(endsWith('abc', 'c', -1)).to.be.false;
+    });
+    // Bigger index
+    it("should return true with params 'abc','abc'", function() {
+        expect(endsWith('abc', 'abc')).to.be.true;
+    });
+    it("should return true with params 'abc','bc'", function() {
+        expect(endsWith('abc', 'bc')).to.be.true;
+    });
+    it("should return false with params 'abc','abcdef'", function() {
+        expect(endsWith('abc', 'abcdef')).to.be.false;
+    });
+});
